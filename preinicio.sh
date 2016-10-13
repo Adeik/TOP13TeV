5a1d63453b86fc35aa88ef3357ee71773712d0c5
Adeik <victoregoslorelua@gmail.com>
2016-10-13 19:00:20 +0200

    Añadido archivo de preinicio

---
+++ preinicio.sh
@@ -0,0 +1,6 @@
+source /cms/slc6_amd64_gcc493/external/gcc/4.9.3/etc/profile.d/init.sh
+source /cms/slc6_amd64_gcc493/external/python/2.7.6/etc/profile.d/init.sh
+source /cms/slc6_amd64_gcc493/external/python/2.7.6/etc/profile.d/dependencies-setup.sh
+source /opt/root6/bin/thisroot.sh
+source /opt/PoD/PoD_env.sh
+source /nfs/fanae/PAF_releases/head/PAF_setup.sh
