613372e3fc521b91ef97589d8b5df54e89fe0d45
Adeik <victoregoslorelua@gmail.com>
2016-10-27 18:31:24 +0200

    Modificadas las condiciones iniciales y el número de cores requeridos para correr los análisis Stop y ttH en Ciencias.

--- preinicio.sh
+++ preinicio.sh
@@ -1,6 +1,3 @@
-source /cms/slc6_amd64_gcc493/external/gcc/4.9.3/etc/profile.d/init.sh
-source /cms/slc6_amd64_gcc493/external/python/2.7.6/etc/profile.d/init.sh
-source /cms/slc6_amd64_gcc493/external/python/2.7.6/etc/profile.d/dependencies-setup.sh
-source /opt/root6/bin/thisroot.sh
+root6
 source /opt/PoD/PoD_env.sh
-source /nfs/fanae/PAF_releases/head/PAF_setup.sh
+source /opt/PAF/PAF_setup.sh
